e3bb6d890660e2d5ec6c07e1a40d4155f06f8769
김효은 <hyoeun@gmail.com>
2024-02-20 18:51:00 +0900

    강의 중간 커밋

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-let news = [];
+let newsList = [];
+
 const getLatesNews = async () => {
     const url = new URL(
         `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines` 
     );
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log('dd', news);
+    newsList = data.articles;
+    render();
+    console.log('dd', newsList);
+}
+
+const render = () => {
+    const newsHTML = newsList.map(
+        (news) => `
+        <div class="row news">
+            <div class="col-lg-4">
+                <img class="news-img-size" src=${news.urlToImage}/>
+            </div>
+            <div class="col-lg-8">
+                <h2>${news.title}</h2>
+                <p>
+                   ${news.description}
+                </p>
+                <div>
+                    ${news.source.name} ${news.publishedAt}
+                </div>
+            </div>
+        </div>
+    `);
+    document.getElementById('news-board').innerHTML = newsHTML
 }
 getLatesNews();
